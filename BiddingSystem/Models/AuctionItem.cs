9ee2f8c7865e4ce09e79ae6f9cf5655ca8e94039
timothyobosi <timothyobosi8@gmail.com>
2024-10-27 02:01:33 +0300

    Model commit

--- BiddingSystem/Models/AuctionItem.cs
+++ BiddingSystem/Models/AuctionItem.cs
@@ -2,5 +2,10 @@
 {
     public class AuctionItem
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal StartingBid {  get; set; }
+        public DateTime EndDate { get; set; }
     }
 }
--- BiddingSystem/Models/Bid.cs
+++ BiddingSystem/Models/Bid.cs
@@ -2,5 +2,13 @@
 {
     public class Bid
     {
+        public int Id { get; set; }
+        public int AutionItemId {  get; set; }
+        public AuctionItem AuctionItem { get; set; }
+        public int UserId {  get; set; }
+        public User User { get; set; }
+
+        public decimal Amount {  get; set; }
+        public DateTime BidDate { get; set; }
     }
 }
